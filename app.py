111eea94eef598d2fbaf1359fa510e2e4bf14063
Shawn Hymers <39200454+shawnhymers@users.noreply.github.com>
2021-04-02 15:37:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-@app.route('/index')
-def hello_world():
-    return render_template('index.html')
+import sys
+sys.path.append('backend')
+import index
+from config import app
+
 if __name__ == '__main__': app.run(host="localhost", port=8000)
