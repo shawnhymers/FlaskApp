177c73d0a78ac29ec05ba3b532d9db28abb7d81c
Shawn Hymers <39200454+shawnhymers@users.noreply.github.com>
2021-04-02 12:07:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def hello_world():
-    render_template('index.html')
+    return render_template('index.html')
 if __name__ == '__main__': app.run(host="localhost", port=8000)
