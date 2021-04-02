1bcfb006f5eb9957f78914409f6f8852f878c703
Shawn Hymers <39200454+shawnhymers@users.noreply.github.com>
2021-04-02 14:51:06 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,20 @@
 
+from flask import Flask
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+# Making an isntance of the Flask Class (THis will get passed to app.py)
+app = Flask(__name__,  template_folder='../templates')
+
+
+# Database config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# Data base object configuration
+class User(db.Model, UserMixin):
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password = db.Column(db.String(60), nullable = False)
