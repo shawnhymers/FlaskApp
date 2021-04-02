a783f196c8cd8438207617cb0197d72841f3c50d
Shawn Hymers <39200454+shawnhymers@users.noreply.github.com>
2021-04-02 14:43:33 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,6 @@
 
+# Importing the flask class
+from flask import Flask
+
+# Making an isntance of the Flask Class (THis will get passed to app.py)
+app = Flask(__name__, template_folder='../templates')
